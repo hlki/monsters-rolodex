ce554e31552515e2a0ffbbd6e50b6630ff71619e
leonid.havrylchyk <leonid.havrylchyk@outlook.com>
2020-03-29 22:23:12 +0300

    Updates

---
+++ precache-manifest.a37c7bedd1e0f60606a36acf26b6194e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885af060326c51fbfb2ff1d50f91821b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ffb351f76a790339eb4",
+    "url": "/monsters-rolodex/static/css/main.2c1ef597.chunk.css"
+  },
+  {
+    "revision": "5aee1ac6ecf24a38689d",
+    "url": "/monsters-rolodex/static/js/2.340cdc49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.340cdc49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffb351f76a790339eb4",
+    "url": "/monsters-rolodex/static/js/main.c51d4672.chunk.js"
+  },
+  {
+    "revision": "94ac101b48457b533709",
+    "url": "/monsters-rolodex/static/js/runtime-main.23655a63.js"
+  }
+]);
